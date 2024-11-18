12f0eed5ebc72207ae92aa9e879b703cde55ab5b
cncsmonster <cncsmonster@163.com>
2024-11-19 00:08:31 +0800

    feat: add --force flag to enable force mode,on which exist file on the path the symlink going to create at will be remove; fix: fix bug occured when try to remove dir,with using fs::remove_dir_all instead of fs::remove_file

--- src/global.rs
+++ src/global.rs
@@ -1,19 +1,33 @@
 static mut DRY_RUN_FLAG: bool = false;
 static mut INTERACTIVE_FLAG: bool = false;
+static mut FORCE_FLAG: bool = false;
 
 pub fn set_dry_run_mode(flag: bool) {
     unsafe {
         DRY_RUN_FLAG = flag;
     }
 }
+
 pub fn set_interactive_mode(flag: bool) {
     unsafe {
         INTERACTIVE_FLAG = flag;
     }
 }
+
+pub fn set_force_mode(flag: bool) {
+    unsafe {
+        FORCE_FLAG = flag;
+    }
+}
+
 pub fn on_dry_run_mode() -> bool {
     unsafe { DRY_RUN_FLAG }
 }
+
 pub fn on_interactive_mod() -> bool {
     unsafe { INTERACTIVE_FLAG }
+}
+
+pub fn on_force_mode() -> bool {
+    unsafe { FORCE_FLAG }
 }
